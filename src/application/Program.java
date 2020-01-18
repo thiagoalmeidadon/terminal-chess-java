b301402667330166c1d89d898a71778a36099e0e
thiagoalmeidadon <thiagodons@gmail.com>
2020-01-18 15:35:30 -0300

    Add classes peças, tabuleiro, imprimindo o tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
