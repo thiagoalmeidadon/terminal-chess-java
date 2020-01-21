706be3955096799220957b90edaf192d858706b8
thiagoalmeidadon <thiagodons@gmail.com>
2020-01-20 22:53:11 -0300

    Add cor as peças, ajustes

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -30,12 +29,32 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	
+	private void placeNewPiece(char column, int row, ChessPiece piece)
+	{
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
+	
 	// metodo responsavel por inciiar a partida colocando as peças no tabuleiro
 	private void initialSetup()
 	{
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-		board.placePiece(new King(board , Color.WHITE), new Position(7,4));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
+		
+		// versao anterior
+		// board.placePiece(new King(board , Color.WHITE), new Position(7,4));
 	}
 	
 }
