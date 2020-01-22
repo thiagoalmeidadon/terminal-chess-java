02082fb291df8f833c53b4c725113538b02f0edc
thiagoalmeidadon <thiagodons@gmail.com>
2020-01-22 19:36:19 -0300

    Implementando classe e metodo abstract, aplicando regras movimento pt1

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -67,6 +67,24 @@ public class Board {
 	}
 	
 	
+	public Piece removePiece(Position position)
+	{
+		if (!positionExists(position))
+		{
+			throw new BoardException("Posição não existe");
+		}
+		if (piece(position) == null)
+		{
+			return null;
+		}
+		Piece aux = piece(position);
+		
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
+	
 	private boolean positionExists(int row, int column)
 	{
 		return row >= 0 && row < rows && column >= 0 && column < columns;
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -16,5 +16,26 @@ public class Piece {
 		return board;
 	}
 	
+	public abstract boolean[][] possibleMoves();
+	
+	// Hook methods 
+	public boolean possibelMove(Position position) 
+	{
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove()
+	{
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++)
+		{
+			for (int j=0; j<mat.length; j++)
+			{
+				if (mat[i][j])
+					return true;
+			}
+		}
+		return false;
+	}
 
 }
